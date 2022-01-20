7072e136bff92fbc8b38fee997944e8aba3537d2
somiyaPrity <somiyakhanp32@gmail.com>
2022-01-21 01:45:28 +0600

    transition added

--- js_file/script.js
+++ js_file/script.js
@@ -7,8 +7,12 @@ let clicks = 1
 toggleBtn.addEventListener('click', () => {
   if (clicks % 2 == 0) {
     navBar.style.display = 'none'
+    navBar.style.opacity = 0
+    navBar.style.transition = 'all 1s ease-in-out'
   } else {
     navBar.style.display = 'block'
+    navBar.style.opacity = 1
+    navBar.style.transition = 'all 1s ease-in-out'
   }
   clicks++
 })
@@ -21,14 +25,15 @@ fetch('js_file/marketing-data.json')
 
 // marketing section ui
 function marketingUI(items) {
-  // marketing section 1
-  const section1Div = document.querySelector('.marketing-section-1')
-  for (item of items.slice(0, 2)) {
-    let div = document.createElement('div')
-    div.className = 'section1-content'
-    div.innerHTML = `<img class="section1-img" src="images/${item.img}" alt="" />
+  if (items.length > 0) {
+    // marketing section 1
+    const section1Div = document.querySelector('.marketing-section-1')
+    for (let item of items.slice(0, 2)) {
+      let div = document.createElement('div')
+      div.className = 'section1-content'
+      div.innerHTML = `<img class="section1-img" src="images/${item.img}" alt="" />
     <div class="section-text">
-      <p class="section-name">${item.name}</p>
+      <p class="section-name">${item.itemName}</p>
       <h2 class="section-description">
         ${item.description}
       </h2>
@@ -36,16 +41,16 @@ function marketingUI(items) {
     
     </div>
           `
-    section1Div.appendChild(div)
-  }
-  // marketing section 2
-  const section2Div = document.querySelector('.marketing-section-2')
-  for (item of items.slice(2, items.length)) {
-    let div = document.createElement('div')
+      section1Div.appendChild(div)
+    }
+    // marketing section 2
+    const section2Div = document.querySelector('.marketing-section-2')
+    for (let item of items.slice(2, items.length)) {
+      let div = document.createElement('div')
 
-    div.innerHTML = `<img class="section2-img" src="images/${item.img}" alt="" />
+      div.innerHTML = `<img class="section2-img" src="images/${item.img}" alt="" />
     <div class="section2-text">
-      <p class="section-name">${item.name}</p>
+      <p class="section-name">${item.itemName}</p>
       <h2 class="section2-description">
         ${item.description}
       </h2>
@@ -53,7 +58,20 @@ function marketingUI(items) {
     
     </div>
           `
-    section2Div.appendChild(div)
+      section2Div.appendChild(div)
+    }
+  } else {
+    console.log('loading....')
   }
 }
 marketingUI()
+
+// ====newsletter email validation=====
+const emailInput = document.querySelector('.email-input').value
+const validateBtn = document.querySelector('#subscribe-btn')
+validateBtn.addEventListener('click', () => {
+  console.log('cliked')
+  // let reg =
+  //   /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  // console.log(reg.test(String(emailInput).toLowerCase()))
+})
